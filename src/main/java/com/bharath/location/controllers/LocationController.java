a8af98a580cc4760c2c51b44ca8fc3c618a75bec
shivendra <shivendra.shivendra@bharatpe.com>
2023-05-13 15:40:18 +0530

    - sending response back to html template - saving data to database using service layer

--- src/main/java/com/bharath/location/controllers/LocationController.java
+++ src/main/java/com/bharath/location/controllers/LocationController.java
@@ -4,8 +4,10 @@ import com.bharath.location.entities.Location;
 import com.bharath.location.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class LocationController {
@@ -24,12 +26,21 @@ public class LocationController {
     }
 
     @RequestMapping("/saveLoc")
-    public String saveLocation(@ModelAttribute("location") Location location) {
+    public String saveLocation(@ModelAttribute("location") Location location, ModelMap modelMap) {
+
         /*
-         *  By default Spring will create a model object, set all the field that come
+         *  By default, Spring will create a model object, set all the field that come
          *  into the request and expose it out as a bean with the following name
          */
-        service.saveLocation(location);
+        Location locationSaved = service.saveLocation(location);
+        String msg = "Location saved with id: " + locationSaved.getId();
+
+        /*
+         *  To send the msg back to the UI: spring allows us do that using a concept called
+         *  model map
+         */
+        modelMap.addAttribute("msg", msg);
+
         return "createLocation";
     }
 
