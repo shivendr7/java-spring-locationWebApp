618603be0cf482054246a14d72c78300ac222453
shivendra <shivendra.shivendra@bharatpe.com>
2023-05-17 16:04:41 +0530

    - adding third party dependency - using JPA query upon database in LocationRepository

--- src/main/java/com/bharath/location/repository/LocationRepository.java
+++ src/main/java/com/bharath/location/repository/LocationRepository.java
@@ -2,7 +2,14 @@ package com.bharath.location.repository;
 
 import com.bharath.location.entities.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LocationRepository extends JpaRepository<Location, Integer> {
+
+    // this query is a JPQL (JPA Query), made against the objects, hibernate will internally convert this to a database query
+    @Query("select type, count(type) from location group by type")
+    public List<Object[]> findTypeAndTypeCount();
 
 }
