2c013a3506d83789dcf173b287589121c9c0f63a
shivendra <shivendra.shivendra@bharatpe.com>
2023-05-29 17:57:43 +0530

    - added functions of create, update, delete and getOneLocation in integration Layer

--- src/main/java/com/bharath/location/controllers/LocationRESTController.java
+++ src/main/java/com/bharath/location/controllers/LocationRESTController.java
@@ -3,11 +3,10 @@ package com.bharath.location.controllers;
 import com.bharath.location.entities.Location;
 import com.bharath.location.repository.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/locations")
@@ -21,4 +20,25 @@ public class LocationRESTController {
         // Spring automatically serializes the list response into json before returning
         return locationRepository.findAll();
     }
+
+    @PostMapping
+    public Location createLocation(@RequestBody Location location) { // request will come as json...spring will deserialize this into location object
+        return locationRepository.save(location);
+    }
+
+    @PutMapping
+    public Location updateLocation(@RequestBody Location location) {
+        return locationRepository.save(location); // if the location already exists, i.e database got a similar id..it will automatically do an update
+    }
+
+    @DeleteMapping("/{id}") // the url will have the id in request
+    public void deleteLocation(@PathVariable("id") int id) { // maps parameter's value to id in request url
+        locationRepository.deleteById(id);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Location> getLocation(@PathVariable("id") int id) {
+        return locationRepository.findById(id);
+    }
+
 }
